113cd93d09db3414732d2767f00c4489ef98b89a
gioitrongxuan <gioi.trongxuan@gmail.com>
2024-06-08 22:40:38 +0700

    add:breeze

--- time-tracker/routes/web.php
+++ time-tracker/routes/web.php
@@ -1,28 +1,27 @@
 <?php
-use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
-Route::get('/home', function () {
-    return view('home');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-// Đoạn mã này sử dụng UserController từ namespace App\Http\Controllers để xử lý các yêu cầu được gửi đến route /users. Cụ thể:
-
-// use App\Http\Controllers\UserController;: Đây là câu lệnh import (import statement) trong PHP. Nó cho phép sử dụng class UserController từ namespace App\Http\Controllers trong file hiện tại.
-
-// Route::resource('users', UserController::class);: Đây là một route declaration trong Laravel. Nó tạo ra các route RESTful cho tài nguyên users, điều này tức là các route cho các hoạt động CRUD (Create, Read, Update, Delete) trên tài nguyên users. Cụ thể, nó tạo ra các route sau:
-
-// GET /users: Hiển thị danh sách người dùng.
-// GET /users/create: Hiển thị form để tạo mới người dùng.
-// POST /users: Lưu thông tin của người dùng mới tạo.
-// GET /users/{id}: Hiển thị thông tin của một người dùng cụ thể.
-// GET /users/{id}/edit: Hiển thị form để chỉnh sửa thông tin của một người dùng.
-// PUT/PATCH /users/{id}: Cập nhật thông tin của một người dùng.
-// DELETE /users/{id}: Xóa một người dùng.
-// Trong đó, UserController::class được sử dụng để xác định controller nào sẽ xử lý các yêu cầu được gửi đến các route này.
-Route::resource('users', UserController::class);
-    
+require __DIR__.'/auth.php';
